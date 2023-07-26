cc3fd1c75713a87bc27ff6823cf209b26bc0b859
pipelonso <andresfelipeibanezcuta2@gmail.com>
2023-07-26 00:15:26 -0500

    Agregar archivos de proyecto.

---
+++ MVVMClass1/Model/RecordatorioM.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+
+namespace MVVMClass1.Model
+{
+    public class RecordatorioM
+    {
+
+        public List<ClRecordatorioEM> GetAllTaskByUserMail(string correo)
+        {
+
+            string procedimiento = "getTaskByMail '" + correo + "'";
+            ClProcesos objSQl = new ClProcesos();
+            DataTable datos = objSQl.mtdConsultas(procedimiento);
+            List<ClRecordatorioEM> listaRecordatorios = new List<ClRecordatorioEM>();   
+
+            for (int i = 0; i < datos.Rows.Count; i++)
+            {
+
+                ClRecordatorioEM objRecordarioEM = new ClRecordatorioEM();
+                objRecordarioEM.IdRecordatorio = int.Parse(datos.Rows[i]["IdRecordatorio"].ToString());
+                objRecordarioEM.Recordatorio = datos.Rows[i]["Recordatorio"].ToString();
+                objRecordarioEM.Fecha = datos.Rows[i]["Fecha"].ToString();
+                objRecordarioEM.Llamado = int.Parse(datos.Rows[i]["Llamado"].ToString());
+                listaRecordatorios.Add(objRecordarioEM);
+
+            }
+
+            return listaRecordatorios;
+
+        }
+
+        public List<List<object>> mtdGetTaskByMailNOENTITY(string correo)
+        {
+
+            string procedimiento = "getTaskByMail '" + correo + "'";
+            ClProcesos objSQl = new ClProcesos();
+            DataTable datos = objSQl.mtdConsultas(procedimiento);
+
+            List<List<object>> listaRecordatorio = new List<List<object>>();
+
+            for (int i = 0; i < datos.Rows.Count; i++)
+            {
+
+                List<object> Recordatorio = new List<object>();
+                Recordatorio.Add(int.Parse(datos.Rows[i]["IdRecordatorio"].ToString()));
+                Recordatorio.Add(datos.Rows[i]["Recordatorio"].ToString());
+                Recordatorio.Add(datos.Rows[i]["Fecha"].ToString());
+                Recordatorio.Add(int.Parse(datos.Rows[i]["Llamado"].ToString()));
+                listaRecordatorio.Add(Recordatorio);
+
+            }
+
+            return listaRecordatorio;
+
+        }
+
+
+    }
+}
---
+++ MVVMClass1/ViewModel/ClRecordatorioVM.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MVVMClass1.Model;
+using Org.BouncyCastle.Asn1.Mozilla;
+
+namespace MVVMClass1.ViewModel
+{
+    public class ClRecordatorioVM
+    {
+
+        public List<ClRecordatorioEVM> mtdGetTaskByUserMail(string correo)
+        {
+
+            RecordatorioM objRecordatorioM = new RecordatorioM();
+            List<ClRecordatorioEM> listaRecordatorioEM = objRecordatorioM.GetAllTaskByUserMail(correo);
+
+            List<ClRecordatorioEVM> listaRecordarioEVM = new List<ClRecordatorioEVM>();
+
+            for (int i = 0; i < listaRecordatorioEM.Count; i++)
+            {
+
+                ClRecordatorioEVM objRecordarioEVM = new ClRecordatorioEVM();
+                objRecordarioEVM.IdRecordatorio = listaRecordatorioEM[i].IdRecordatorio;
+                objRecordarioEVM.Recordatorio = listaRecordatorioEM[i].Recordatorio;
+                objRecordarioEVM.Fecha = listaRecordatorioEM[i].Fecha;
+                objRecordarioEVM.Llamado = listaRecordatorioEM[i].Llamado;
+
+                listaRecordarioEVM.Add(objRecordarioEVM);
+
+            }
+
+            return listaRecordarioEVM;
+
+        }
+
+        public List<ClRecordatorioEVM> mtdGetAllTaskByMailNOENTITY(string correo)
+        {
+
+            RecordatorioM objRecordatorioM = new RecordatorioM();
+            List<List<object>> listaRecordatorio = objRecordatorioM.mtdGetTaskByMailNOENTITY(correo);
+
+            List<ClRecordatorioEVM> listaRecordatorioEVM = new List<ClRecordatorioEVM>();
+
+            for (int i = 0; i < listaRecordatorio.Count; i++)
+            {
+
+                ClRecordatorioEVM objRecordatorioEVM = new ClRecordatorioEVM();
+                objRecordatorioEVM.IdRecordatorio = int.Parse(listaRecordatorio[i][0].ToString());
+                objRecordatorioEVM.Recordatorio = listaRecordatorio[i][1].ToString();
+                objRecordatorioEVM.Fecha = listaRecordatorio[i][2].ToString();
+                objRecordatorioEVM.Llamado = int.Parse(listaRecordatorio[i][3].ToString());
+                listaRecordatorioEVM.Add(objRecordatorioEVM);
+
+            }
+
+            return listaRecordatorioEVM;
+
+        }
+
+
+
+    }
+}
